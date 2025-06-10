a72293fd499839e10c93649099690ab157d791c2
Renato A. Silva <araujo.renato@unemat.br>
2025-06-09 22:22:26 -0400

    Fist Init

---
+++ Sistema-de-Gerenciamento-de-Empregado/src/main/java/com/renatoas/employeemanagementsystem/SistemaDeGerenciamentoDeEmpregadoApplication.java
@@ -0,0 +1,13 @@
+package com.renatoas.employeemanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeGerenciamentoDeEmpregadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDeGerenciamentoDeEmpregadoApplication.class, args);
+	}
+
+}
