2a73a0ddb933325c4d39cccf6509e8d327e5637b
Renato Silva <araujo.renato@unemat.br>
2025-06-14 18:50:18 -0400

    Criação do DTO para Employee

--- Sistema-de-Gerenciamento-de-Empregado/src/main/java/com/renatoas/employeemanagementsystem/dto/EmployeeDto.java
+++ Sistema-de-Gerenciamento-de-Empregado/src/main/java/com/renatoas/employeemanagementsystem/dto/EmployeeDto.java
@@ -1,5 +1,15 @@
 package com.renatoas.employeemanagementsystem.dto;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class EmployeeDto {
     private Long id;
     private String firstName;
