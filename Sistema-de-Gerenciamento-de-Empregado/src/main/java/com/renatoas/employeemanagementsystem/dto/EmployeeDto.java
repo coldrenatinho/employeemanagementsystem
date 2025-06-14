9d832afc50f2be74bc29684722daa9350cbb3cb3
Renato Silva <araujo.renato@unemat.br>
2025-06-14 18:33:59 -0400

    Criação do DTO para os empregados

--- Sistema-de-Gerenciamento-de-Empregado/src/main/java/com/renatoas/employeemanagementsystem/dto/EmployeeDto.java
+++ Sistema-de-Gerenciamento-de-Empregado/src/main/java/com/renatoas/employeemanagementsystem/dto/EmployeeDto.java
@@ -1,4 +1,8 @@
 package com.renatoas.employeemanagementsystem.dto;
 
 public class EmployeeDto {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
 }
