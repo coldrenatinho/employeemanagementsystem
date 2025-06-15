9130a87d29617b16be7e786f40949bddaf087f92
Renato Silva <araujo.renato@unemat.br>
2025-06-15 10:22:21 -0400

    Criacao de um classe para tratar a exeções personalida de execução

---
+++ Sistema-de-Gerenciamento-de-Empregado/src/main/java/com/renatoas/employeemanagementsystem/exception/ResourceNotFoundException.java
@@ -0,0 +1,17 @@
+// Exceção personalizada para recursos não encontrados.
+// Quando lançada, retorna o status HTTP 404 (NOT_FOUND).
+package com.renatoas.employeemanagementsystem.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+// Anotação que define o status HTTP 404 para esta exceção.
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends RuntimeException {
+
+    // Construtor que recebe uma mensagem de erro personalizada e retorna a superclasse RuntimeException.
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+
+}
