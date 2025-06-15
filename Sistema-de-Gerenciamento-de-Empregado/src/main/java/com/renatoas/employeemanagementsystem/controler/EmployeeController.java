060e7c6145f594efbd33b5e1f76d643609eb944a
Renato Silva <araujo.renato@unemat.br>
2025-06-14 23:11:31 -0400

    ## Verificar vilidações de dados em Java

--- Sistema-de-Gerenciamento-de-Empregado/src/main/java/com/renatoas/employeemanagementsystem/controler/EmployeeController.java
+++ Sistema-de-Gerenciamento-de-Empregado/src/main/java/com/renatoas/employeemanagementsystem/controler/EmployeeController.java
@@ -1,4 +1,25 @@
 package com.renatoas.employeemanagementsystem.controler;
 
+import com.renatoas.employeemanagementsystem.dto.EmployeeDto;
+import com.renatoas.employeemanagementsystem.service.EmployeeService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/employees")
+@AllArgsConstructor
 public class EmployeeController {
+
+
+    private EmployeeService employeeService;
+
+
+    // Define endpoints for employee management here
+    public ResponseEntity<EmployeeDto> createEmployee(@RequestBody EmployeeDto employeeDto) {
+        EmployeeDto createdEmployee = employeeService.createEmployee(employeeDto);
+    }
+
 }
