203f53b24a3b852358350fa7c69ae626e9cfb79c
Renato Silva <araujo.renato@unemat.br>
2025-06-15 00:11:09 -0400

    Criação da API na Classe de controlador

--- Sistema-de-Gerenciamento-de-Empregado/src/main/java/com/renatoas/employeemanagementsystem/controler/EmployeeController.java
+++ Sistema-de-Gerenciamento-de-Empregado/src/main/java/com/renatoas/employeemanagementsystem/controler/EmployeeController.java
@@ -3,7 +3,10 @@ package com.renatoas.employeemanagementsystem.controler;
 import com.renatoas.employeemanagementsystem.dto.EmployeeDto;
 import com.renatoas.employeemanagementsystem.service.EmployeeService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,9 +20,13 @@ public class EmployeeController {
     private EmployeeService employeeService;
 
 
+    @PostMapping
     // Define endpoints for employee management here
     public ResponseEntity<EmployeeDto> createEmployee(@RequestBody EmployeeDto employeeDto) {
-        EmployeeDto createdEmployee = employeeService.createEmployee(employeeDto);
+        EmployeeDto savedEmployee = employeeService.createEmployee(employeeDto);
+        // Return the created employee with a 201 Created status
+        //Verificar
+        return new ResponseEntity<>(savedEmployee, HttpStatus.CREATED);
     }
 
 }
