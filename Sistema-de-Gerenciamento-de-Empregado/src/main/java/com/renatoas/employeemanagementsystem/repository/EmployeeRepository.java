115e381a589e95c8ce22a281ffe2a25398878215
Renato Silva <araujo.renato@unemat.br>
2025-06-14 23:12:58 -0400

    Correção do Repositorio que não estava inferindo o Jpa

--- Sistema-de-Gerenciamento-de-Empregado/src/main/java/com/renatoas/employeemanagementsystem/repository/EmployeeRepository.java
+++ Sistema-de-Gerenciamento-de-Empregado/src/main/java/com/renatoas/employeemanagementsystem/repository/EmployeeRepository.java
@@ -1,4 +1,7 @@
 package com.renatoas.employeemanagementsystem.repository;
 
-public interface EmployeeRepository {
+import com.renatoas.employeemanagementsystem.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 }
